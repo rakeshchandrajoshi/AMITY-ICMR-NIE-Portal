5b637d45c52c909021c623e383fa01e2242a5e07
kalpokoch <kalpokoch@gmail.com>
2026-03-12 14:17:28 +0530

    changes to age description

--- app.py
+++ app.py
@@ -184,7 +184,7 @@ def main():
         patient_data = {}
 
         # Demographics (MATCH EXACT TRAINING COLUMN NAMES)
-        patient_data['age'] = st.sidebar.number_input("Age(less than 0 will be taken as months)", min_value=0.0, max_value=120.0, value=30.0, step=0.1)
+        patient_data['age'] = st.sidebar.number_input("Age(If age is less than 1, enter 0)", min_value=0.0, max_value=120.0, value=30.0, step=0.1)
         patient_data['SEX'] = st.sidebar.selectbox("Sex", options=[0, 1], 
                                                     format_func=lambda x: "Female" if x == 0 else "Male", index=1)
         patient_data['PATIENTTYPE'] = st.sidebar.selectbox("Patient Type", options=[0, 1], 
